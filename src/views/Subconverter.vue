b7e2e5d0f40d155cd65419425457663845c3d982
Yue Wang <xfdywy@users.noreply.github.com>
2020-06-06 18:35:56 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -219,7 +219,7 @@ const project = "https://github.com/CareyWang/sub-web";
 const remoteConfigSample =
   "https://raw.githubusercontent.com/tindy2013/subconverter/master/base/config/example_external_config.ini";
 const gayhubRelease = "https://github.com/tindy2013/subconverter/releases";
-const defaultBackend = "http://aly.yue-shuang.fun:25500/sub?new_name=false&";
+const defaultBackend = "http://txy.wyue.site:25500/sub?new_name=false&";
 const shortUrlBackend = "https://api.suo.yt/short";
 const configUploadBackend = "https://api.wcc.best/config/upload";
 const tgBotLink = "https://t.me/CareyWong_bot";
@@ -249,15 +249,20 @@ export default {
           ssd: "ssd"
         },
         backendOptions: [{ value: "http://127.0.0.1:25500/sub?" },
-                        { value: "http://aly.yue-shuang.fun:25500/sub?new_name=false&" },
-                        { value: "http://aly.yue-shuang.fun:25500/sub?new_name=true&" },
-                        { value: "https://api.wcc.best/sub?new_name=false&"},
-                        { value: "https://api.wcc.best/sub?new_name=true&"},
+                        { value: "http://txy.wyue.site:25500/sub?new_name=false&" },
+                        { value: "http://txy.wyue.site:25500/sub?new_name=true&" },
+                        { value: "https://sub.bianyuan.xyz/sub?new_name=false&"},
+                        { value: "https://sub.bianyuan.xyz/sub?new_name=true&"},
                         ],
         remoteConfig: [
           {
             label: "universal",
             options: [
+               {
+                label: "myself",
+                value:
+                  "https://raw.githubusercontent.com/xfdywy/sub-web/master/config/myconfig1"
+              },
               {
                 label: "No-Urltest",
                 value:
